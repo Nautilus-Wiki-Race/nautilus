1a85341a439fd8a277d509cec967ad98a74b8763
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-22 14:44:37 -0700

    added Julias integrations

--- main_app.py
+++ main_app.py
@@ -4,11 +4,36 @@ wiki race flask app
 """
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import json
+import requests
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 host = '0.0.0.0'
 port = 5000
+
+
+def search_wiki(page1, page2):
+    """
+    basic search for pages
+    """
+    page1 = page1.replace(' ', '_')
+    wiki_url = "https://en.wikipedia.org/wiki/"
+    url = "https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=500&format=json&titles=" + page1
+    r = requests.get(url, headers={'User-agent': 'holberton 0.1'})
+    pages = r.json().get("query").get("pages")
+    for k, v in pages.items():
+        k = k
+        v = v
+    links = v.get("links")
+    titles = []
+    for link in links:
+        titles.append(link.get('title'))
+    if page2 in titles:
+        page_2 = page2.replace(' ', '_')
+        ret_url = wiki_url + page_2
+        return(["good job", "the link is on the same page"])
+    else:
+        return(["error", "link not found"])
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,12 +46,7 @@ def index():
     if request.method == 'POST':
         page_one = request.form['PAGE_ONE']
         page_two = request.form['PAGE_TWO']
-        print(page_one)
-        print(page_two)
-        results_obj = [
-            page_one,
-            page_two
-        ]
+        results_obj = search_wiki(page_one, page_two)
         return redirect(url_for('results', results_obj=json.dumps(results_obj)))
 
 
